ae1c470aaf3f2f59cd7c99b5e5128dc04302be3e
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2022-12-14 21:33:28 +0900

    init

---
+++ works/work1/sketch.js
@@ -0,0 +1,35 @@
+let tiles = [];
+
+function setup() {
+  let boundingRects = document
+    .getElementById("p5Canvas")
+    .getBoundingClientRect();
+  let canvas = createCanvas(boundingRects.width, boundingRects.height);
+  canvas.parent("p5Canvas");
+  let tileWidth = width / (howManyX + 1);
+  let tileHeight = height / (howManyY + 1);
+  for (let tileCntX = 0; tileCntX < howManyX; tileCntX++) {
+    for (let tileCntY = 0; tileCntY < howManyY; tileCntY++) {
+      let tileX = tileWidth * (tileCntX + 1) - tileWidth * 0.5;
+      let tileY = tileHeight * (tileCntY + 1) - tileHeight * 0.5;
+      let tile = new Tile(tileX, tileY, tileWidth, tileHeight);
+      tiles.push(tile);
+    }
+  }
+}
+
+let howManyX = 20;
+let howManyY = 20;
+
+function mousePressed() {
+  tiles.forEach((tile) => {
+    tile.setStateByMouse(mouseX, mouseY);
+  });
+}
+
+function draw() {
+  background(255);
+  tiles.forEach((tile) => {
+    tile.render();
+  });
+}
